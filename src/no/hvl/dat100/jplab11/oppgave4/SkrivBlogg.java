56ba06ba987e9a20680f90d7b0a17186b94743eb
Birk <birk.johannessen@hotmail.com>
2020-10-26 21:04:33 +0100

    ferdig med oblig i oppgaven, noe må endres, som er markert som dårlig kode..

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -2,6 +2,7 @@ package no.hvl.dat100.jplab11.oppgave4;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.io.File;
 
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave3.*;
@@ -9,7 +10,26 @@ import no.hvl.dat100.jplab11.oppgave3.*;
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+		boolean skrivd=false;
+		PrintWriter skriver=null;
+		try {
+			String ut=samling.toString();
+			skriver = new PrintWriter(mappe+filnavn);
+			
+			skriver.print(ut);
+			
+		}
+		catch(FileNotFoundException e){
+			System.out.println("feil: Filen kan ikke åpnes");
+		}
+		finally {
+			if(skriver != null) {
+				skriver.close();
+				skrivd=true;
+			}
+			
+		}
+		return skrivd;
 
-		throw new UnsupportedOperationException(TODO.method());
 	}
 }
